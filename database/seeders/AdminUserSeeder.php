0a2dfa9f5e84b62aac0512f8dcb38866f64d385a
M.Farhan <100473373+M-Farhan-Shaukat@users.noreply.github.com>
2026-04-01 09:58:43 +0500

    Initial commit

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AdminUserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::updateOrCreate(
+            ['email' => 'admin@gmail.com'],
+            [
+                'name'     => 'Admin',
+                'password' => Hash::make('Temp123!'),
+                'role_id'     => 1,
+                'email_verified_at' => now(),
+                'is_active' => true,
+            ]
+        );
+        User::updateOrCreate(
+            ['email' => 'user@gmail.com'],
+            [
+                'name'     => 'User',
+                'password' => Hash::make('Temp123!'),
+                'role_id'     => 2,
+                'email_verified_at' => now(),
+                'is_active' => true,
+            ]
+        );
+    }
+}
